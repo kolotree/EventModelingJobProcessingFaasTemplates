b1e66620eef6c4bc64611cc9dd6bbcc8e00af7c3
Stanko Culaja <stanko.culaja@kolotree.com>
2021-02-09 11:51:54 +0100

    Added test web app to the template.

---
+++ template/csharp-http-command/function/Function.WebApp/Controllers/FunctionController.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using JobProcessing.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Function.WebApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FunctionController : ControllerBase
+    {
+        private readonly IStore _store;
+
+        public FunctionController(IStore store)
+        {
+            _store = store;
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> Post()
+        {
+            var functionResult = await new FunctionHandler(_store).Handle(HttpContext.Request);
+            return new JsonResult(functionResult) {StatusCode = functionResult.StatusCode};
+        }
+    }
+}
---
+++ template/csharp-http-command/function/Function.WebApp/Startup.cs
@@ -0,0 +1,51 @@
+using System;
+using JobProcessing.Abstractions;
+using JobProcessing.Infrastructure.EventStore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Function.WebApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            
+            var store = EventStoreBuilder
+                .NewUsing(
+                    Environment.GetEnvironmentVariable("EventStore_ConnectionString") 
+                    ?? throw new ArgumentException("EventStore connection string is not provided through environment variable 'EventStore_ConnectionString'."))
+                .NewStore();
+            services.Add(new ServiceDescriptor(typeof(IStore), store));       
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        }
+    }
+}
